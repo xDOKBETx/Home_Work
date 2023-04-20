e6209ac8dfd1af46c570b4c000abe4199c415e63
Aleksey Valentinov <ds83351@chebnet.com>
2023-04-20 23:53:13 +0300

    Update README

--- task62/Program.cs
+++ task62/Program.cs
@@ -1,74 +1,71 @@
 ﻿using System;
 
-internal class NewBaseType
+class Program
 {
+    static void Main()
+    {
+        int[,] spiralArray = GetSpiral(4, 4); // Создание спирального массива 4x4
+        PrintArray(spiralArray); // Вывод спирального массива в консоль
+    }
+
+    static int[,] GetSpiral(int rows, int columns)
+    {
+        int[,] array = new int[rows, columns]; // Создание пустого массива нужной размерности
+        int value = 1; // Начальное значение для заполнения массива
+        int startRow = 0; // Начальный индекс строки
+        int endRow = rows - 1; // Конечный индекс строки
+        int startColumn = 0; // Начальный индекс столбца
+        int endColumn = columns - 1; // Конечный индекс столбца
+
+        while (startRow <= endRow && startColumn <= endColumn)
+        {
+            // Заполнение верхней строки слева направо
+            for (int i = startColumn; i <= endColumn; i++)
+            {
+                array[startRow, i] = value++;
+            }
+            startRow++; // Переход на следующую строку
+
+            // Заполнение правого столбца сверху вниз
+            for (int i = startRow; i <= endRow; i++)
+            {
+                array[i, endColumn] = value++;
+            }
+            endColumn--; // Переход на предыдущий столбец
+
+            if (startRow <= endRow)
+            {
+                // Заполнение нижней строки справа налево
+                for (int i = endColumn; i >= startColumn; i--)
+                {
+                    array[endRow, i] = value++;
+                }
+                endRow--; // Переход на предыдущую строку
+            }
+
+            if (startColumn <= endColumn)
+            {
+                // Заполнение левого столбца снизу вверх
+                for (int i = endRow; i >= startRow; i--)
+                {
+                    array[i, startColumn] = value++;
+                }
+                startColumn++; // Переход на следующий столбец
+            }
+        }
+
+        return array; // Возврат заполненного спирального массива
+    }
+
     static void PrintArray(int[,] array)
     {
         for (int i = 0; i < array.GetLength(0); i++)
         {
             for (int j = 0; j < array.GetLength(1); j++)
             {
-                Console.Write(array[i, j].ToString("D2") + " ");
+                Console.Write(array[i, j].ToString("D2") + " "); // Вывод элемента массива с дополнением до 2 знаков
             }
-            Console.WriteLine();
+            Console.WriteLine(); // Переход на новую строку после вывода каждой строки массива
         }
-    }
-}
-
-class Program : NewBaseType
-{
-    static void Main()
-    {
-        int[,] myArray = GetSpiral(4, 4);
-        PrintArray(myArray);
-    }
-
-    static int[,] GetSpiral(int rows, int columns)
-    {
-        int[,] array = new int[rows, columns];
-        int value = 1;
-        int startRow = 0;
-        int endRow = rows - 1;
-        int startColumn = 0;
-        int endColumn = columns - 1;
-
-        while (startRow <= endRow && startColumn <= endColumn)
-        {
-            // Fill the top row
-            for (int i = startColumn; i <= endColumn; i++)
-            {
-                array[startRow, i] = value++;
-            }
-            startRow++;
-
-            // Fill the right column
-            for (int i = startRow; i <= endRow; i++)
-            {
-                array[i, endColumn] = value++;
-            }
-            endColumn--;
-
-            if (startRow <= endRow)
-            {
-                // Fill the bottom row
-                for (int i = endColumn; i >= startColumn; i--)
-                {
-                    array[endRow, i] = value++;
-                }
-                endRow--;
-            }
-
-            if (startColumn <= endColumn)
-            {
-                // Fill the left column
-                for (int i = endRow; i >= startRow; i--)
-                {
-                    array[i, startColumn] = value++;
-                }
-                startColumn++;
-            }
-        }
-
-        return array;
     }
 }
